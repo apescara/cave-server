5b246e1cfc52a2e323b7d9d897725a12189642db
Andres Pescara <andrespescara@gmail.com>
2026-02-05 21:00:12 -0300

    add IaC with TF

---
+++ iac/100.tf
@@ -0,0 +1,47 @@
+resource "proxmox_lxc_guest" "basic" {
+  guest_id = 100
+  name     = "filebrowser"
+  target_node  = "cave"
+  password     = var.lxc_password
+  unprivileged = true
+
+  features {
+        unprivileged {
+            nesting = true
+        }
+    }
+
+  template {
+        file    = "debian-13-standard_13.1-2_amd64.tar.zst"
+        storage = "local"
+    }
+
+  cpu {
+        cores = 1
+    }
+
+  memory = 512
+  swap = 512
+
+  // Terraform will crash without rootfs defined
+  root_mount  {
+    storage = "local-lvm"
+    size    = "8G"
+  }
+
+  network {
+    id = 0
+    name   = "eth0"
+    bridge = "vmbr0"
+    ipv4_dhcp     = true
+    ipv6_dhcp     = true
+  }
+
+
+  mount {
+    slot    = "mp0"
+    host_path = "/lake1t/data"
+    guest_path      = "/mnt/lake1t"
+    type= "bind"
+  }
+}
