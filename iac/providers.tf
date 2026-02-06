afd8763d7eb8dbcbff2c661f828a617f4398f1df
Andres Pescara - Proxmox <andrespescara@gmail.com>
2026-02-05 21:13:23 -0300

    make 100 as test

--- iac/providers.tf
+++ iac/providers.tf
@@ -1,7 +1,7 @@
 provider "proxmox" {
-  pm_api_url = "https://192.168.100.17:8006/api2/json"
+  pm_api_url = "https://localhost:8006/api2/json"
   pm_tls_insecure = true
-  pm_user = var.user
-  pm_password= var.password
+  pm_user = var.pm_user
+  pm_password= var.pm_password
   pm_minimum_permission_check = false
 }
---
+++ iac/variables.tf
@@ -0,0 +1,19 @@
+variable "pm_user" {
+  type = string
+}
+
+variable "pm_password" {
+  type = string
+}
+
+variable "pm_api_token_id" {
+  type = string
+}
+
+variable "pm_api_token_secret" {
+  type = string
+}
+
+variable "lxc_password" {
+  type = string
+}
